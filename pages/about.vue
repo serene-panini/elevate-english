ae73c8cebd49d42a90fb82f965abef462abdea4b
serene-panini <contact.elevateenglish@gmail.com>
2020-04-17 10:52:43 -0700

    tried to fix boxes by changing div tag positions

--- pages/about.vue
+++ pages/about.vue
@@ -25,29 +25,30 @@
               Read full post to learn more about the Elevate Method
             </strong>
           </p>
-
-          <div class="title is-ancestor">
-            <div class="title is-parent">
-              <article class="title is-child box">
-                <img src="../assets/uploads/contact.jpg" height="250"/>
-              </article>
-            </div>
-            <div class="title is-parent">
-              <article class="title is-child box">
-                <p class="title">
-                  About Anne
-                </p>
-                <p class="subtitle">
-                  BA Psychology, University of British Colombia
-                </p>
-                <p>
-                  I grew up in Seattle, in the United States. After high school I moved to Vancouver, Canada to study Psychology. I've loved traveling and learning languages since I was young. I got my certificate in Teaching English as a Foreign Language (TEFL) after graduation, and started traveling the world while teaching English. Six months ago I moved to France. I studied French in high school, but I didn't feel like I learned the language well with only grammar and vocabulary. Instead, I improved my French naturally by listening to podcasts, watching YouTube videos and TV shows, and by talking to new friends French, even though it was scary at first. I want to help my English students in the same way. Even if they can't move to an English speaking country, my podcasts, YouTube channel, and blog posts can help them learn English naturally at home.
-                </p>
-              </article>
-            </div>
-          <quote-card/>
         </div>
 
+        <div class="title is-ancestor">
+          <div class="title is-parent">
+            <article class="title is-child box">
+              <img src="../assets/uploads/contact.jpg" height="250"/>
+            </article>
+          </div>
+          <div class="title is-parent">
+            <article class="title is-child box">
+              <p class="title">
+                About Anne
+              </p>
+              <p class="subtitle">
+                 BA Psychology, University of British Colombia
+              </p>
+              <p>
+                I grew up in Seattle, in the United States. After high school I moved to Vancouver, Canada to study Psychology. I've loved traveling and learning languages since I was young. I got my certificate in Teaching English as a Foreign Language (TEFL) after graduation, and started traveling the world while teaching English. Six months ago I moved to France. I studied French in high school, but I didn't feel like I learned the language well with only grammar and vocabulary. Instead, I improved my French naturally by listening to podcasts, watching YouTube videos and TV shows, and by talking to new friends French, even though it was scary at first. I want to help my English students in the same way. Even if they can't move to an English speaking country, my podcasts, YouTube channel, and blog posts can help them learn English naturally at home.
+              </p>
+            </article>
+          </div>
+        </div>
+        <quote-card/>
+        
         <div class="tile is-ancestor">
           <div class="tile is-parent">
             <article class="tile is-child box">
