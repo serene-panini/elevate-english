3430e49e3bcf146ff328a5f9933b754505a869a0
serene-panini <contact.elevateenglish@gmail.com>
2020-04-19 15:55:14 -0700

    added quote card 2

---
+++ components/cards/QuoteCardTwo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="card quote-card-two">
+    <div class="card-content">
+      <p class="title">
+        “Listening is not the same as hearing and hearing is not the same as listening”
+      </p>
+      <p class="subtitle">
+        ~ Pauline Oliveros
+      </p>
+    </div>
+  </div>
+</template>
