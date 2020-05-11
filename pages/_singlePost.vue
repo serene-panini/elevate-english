40cde65bf004b5f2e2f0601469497c84540cd891
serene-panini <contact.elevateenglish@gmail.com>
2020-05-11 15:12:57 -0700

    tried to add dictionary form

--- pages/_singlePost.vue
+++ pages/_singlePost.vue
@@ -11,6 +11,7 @@
         <strong>Published on:</strong> {{ date }}
       </span>
     </site-hero>
+    <form action='https://dictionary.cambridge.org/search/british/direct/?utm_source=widget_searchbox_source&utm_medium=widget_searchbox&utm_campaign=widget_tracking' method='post'> <table style='font-size:10px;background:#292929;border-collapse:collapse;border-spacing:0;width:150px;' > <tbody> <tr> <td colspan='2' style='padding:0;background:none;border:none;'> <a href='https://dictionary.cambridge.org/' style='display:block; background: transparent url(/external/images/freesearch/sbl.png?version=5.0.93) no-repeat 5px 6px;height:32px;'></a> </td> </tr> <tr> <td style='width:68px;background:none;border:none;padding:4px;'> <input style='width:100%;display:block;font-size:10px;padding:2px;border:none;' name='q' /> </td> <td style='width:50px;background:none;border:none;padding:0 4px 0 0;'> <input style='width:100%;display:block;font-size:10px;padding:2px;border:none;float:right;background:#d0a44c;' type='submit' value='Look it up' /> </td> </tr> </tbody> </table> </form>
     <main-section :one-column-constrained="true">
       <template v-slot:default>
         <div class="post-wrapper">
