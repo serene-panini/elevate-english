3a26edfb13c1a99345083efb6b80a08875b7f67e
serene-panini <contact.elevateenglish@gmail.com>
2020-05-19 10:01:38 -0700

    added my google analytics id

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -15,7 +15,7 @@ export default {
   logo: '/logo.png', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
-    id: process.env.GOOGLE_ANALYTICS_ID
+    id: '167138847-1'
   },
   mainMenu: [
     {
