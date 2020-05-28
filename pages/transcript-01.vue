e313cb04a24f9464ee6e4251feba5c5eab0682a6
serene-panini <contact.elevateenglish@gmail.com>
2020-05-27 18:25:29 -0700

    tried to make player sticky

--- pages/transcript-01.vue
+++ pages/transcript-01.vue
@@ -2,9 +2,11 @@
   <div id="about-page" class="page-wrapper about-page content-page">
     <main-section theme="sidebar-right">
       <template v-slot:default>
+        <div class="fixed-container">
+            <div id="buzzsprout-player-3941621"></div>
+            <script src="https://www.buzzsprout.com/1122713/3941621-the-psychology-of-language-learning.js?container_id=buzzsprout-player-3941621&player=small" type="text/javascript" charset="utf-8"></script>
+        </div>
         <div class="content">
-        <div id="buzzsprout-player-3941621"></div>
-        <script src="https://www.buzzsprout.com/1122713/3941621-the-psychology-of-language-learning.js?container_id=buzzsprout-player-3941621&player=small" type="text/javascript" charset="utf-8"></script>
           <h3>Episode 01 Transcript</h3>
           <p>
             0:00 Hey everyone! It’s Anne from Elevate English. Thank you so much for being here today for my first episode!
