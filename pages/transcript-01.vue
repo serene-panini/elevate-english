9d81fdf66c7c11e2aabbfc4ab8dcf13b24581ec9
serene-panini <contact.elevateenglish@gmail.com>
2020-05-27 17:31:03 -0700

    buzzsprout player embed

--- pages/transcript-01.vue
+++ pages/transcript-01.vue
@@ -3,6 +3,8 @@
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
+        <div id="buzzsprout-player-3941621"></div>
+        <script src="https://www.buzzsprout.com/1122713/3941621-the-psychology-of-language-learning.js?container_id=buzzsprout-player-3941621&player=small" type="text/javascript" charset="utf-8"></script>
           <h3>Episode 01 The Psychology of Language Learning</h3>
           <p>
             Hey everyone! It’s Anne from Elevate English. Thank you so much for being here today for my first episode!
