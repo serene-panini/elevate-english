a5e4bb57e431b0a278d63d8a41e4195fbf57912c
serene-panini <contact.elevateenglish@gmail.com>
2020-06-02 16:09:25 -0700

    new podcast ep

---
+++ pages/transcript-blue-moon.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="about-page" class="page-wrapper about-page content-page">
+    <main-section theme="sidebar-right">
+      <template v-slot:default>
+        <div class="content">
+            <div>
+            <div id="buzzsprout-player-4020641"></div>
+            <script src="https://www.buzzsprout.com/1122713/4020641-once-in-a-blue-moon.js?container_id=buzzsprout-player-4020641&player=small" type="text/javascript" charset="utf-8"></script>
+            </div>
+            
+          <h3>Once in a Blue Moon Transcript</h3>
+          <p>
+            Hey everyone! It’s Anne from Elevate English. 
+          </p>
+          <p>
+            00:04 Today I’m going to explain a pretty common expression we have in English, “once in a blue moon”. To quote a great old song that I can’t actually play here for copyright reasons, “once in a blue moon something good comes along”. 
+          </p>
+          <p>
+            00:18 The meaning of this expression is something that happens very rarely. Something that almost never happens. Something unlikely. So the lyric “once in a blue moon something good comes along” means good things happen rarely, or don’t happen very often. Another example could be, “In my country, earthquakes happen only once in a blue moon”. You’re saying that earthquakes aren’t very common where you live. 
+          </p>
+          <p>
+            00:42 This expression is quite old. Most sources I found online say it’s been in use since the 1800’s. Historically, there are some very rare examples of the moon actually appearing blue, caused by unusual weather or a volcanic eruption. But that’s probably not where this expression comes from. It doesn’t actually have to do with the color of the moon. “Blue moon” is what it’s called when a full moon happens a second time in the same month of the year. The average time between full moons is about 29.53 days, while the average length of a month is 30.4 days. This means that although it’s pretty uncommon for one month to have two full moons, every so often it does happen.  
+          </p>
+          <p>
+            01:24 Some people calculated that the mathematical probability of a second full moon occurring is 8 chances out of 228. So that means technically if you say something happens once in a blue moon, you’re saying there is a 3.5% chance of that thing happening! Unlikely, but not impossible.
+          </p>
+          <p>
+            01:45 If listening to music helps you in your language learning, I definitely recommend looking up Van Morrison’s song called “Once in a Blue Moon” to help remember this expression. I put a link to the song in the description. Thanks for listening! Have a great day guys.
+          </p>
+        </div>
+      </template>
+    </main-section>
+  </div>
+
+</template>
